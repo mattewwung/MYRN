3327979f61e39ce08c49f93a4c4ffbaefb2b7a89
WANG XIAO YU <mattewwung@gmail.com>
2026-03-23 11:32:15 +0800

    Remove unused useSafeAreaInsets import from App component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { StatusBar, StyleSheet, useColorScheme, View, TouchableOpacity, Text } from 'react-native';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
 function App() {
@@ -16,8 +15,6 @@ function App() {
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
   return (
     <View style={styles.container}>
       <TouchableOpacity style={{ backgroundColor: 'blue', padding: 10, marginBottom: 10 }}>
